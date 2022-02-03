bf2397963df9830f95fd0ca6933b9965b3f8c5c8
herwidodo <herwidodo@gdn-commerce.com>
2022-02-03 15:43:56 +0700

    init coverage jest unit test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/api-mock/*.{js}',
+    'src/store/*.{js}',
+    'src/components/**.{vue}',
+    'src/views/**.{vue}',
+    '!**/node_modules/**'
+  ],
+  coverageDirectory: 'tests/unit/coverage',
+  coverageReporters: [
+    'lcov',
+    'text',
+    'json'
+  ],
+  testMatch: [
+    '<rootDir>/tests/unit/**/*(*.)@(spec|test).[jt]s?(x)'
+  ],
+  testURL: 'http://localhost'
 }
